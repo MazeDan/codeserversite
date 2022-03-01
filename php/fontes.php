e0762469a79a100139f9f962f53117e6e2f5713b
Daniel <88732777+MazeDan@users.noreply.github.com>
2022-03-01 11:49:07 -0300

    first commit

---
+++ php/fontes.php
@@ -0,0 +1,8 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto&family=Shadows+Into+Light&display=swap" rel="stylesheet"> 
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Slab:wght@800&family=Shadows+Into+Light&display=swap" rel="stylesheet"> 
---
+++ php/menu.php
@@ -0,0 +1,17 @@
+    <header class="p-3 text-white" style="background-color: #6e63ed ;">
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"> 
+            <li><a href="../arquivos/scan.php" class="nav-link px-3 text-white" 
+style="font-family: 'Roboto', sans-serif;
+font-family: 'Roboto Slab', serif;font-family: 'Shadows Into Light', cursive;"><i class="bi bi-home"></i> Michelle Silva</a></li>
+            <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-person-circle"></i> Redes Socias</a></li>
+            <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-mailbox2"></i> FAQs</a></li>
+            <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-pc-display-horizontal"></i> Sobre</a></li>
+            </ul>
+
+            <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">Login</button>
+            <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
+        </div>
+    </header> 
